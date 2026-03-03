23f6439465472bd4836cd8affc43d5f0eed2f0f2
Linashihova <linashikhova@mail.ru.>
2026-03-03 14:49:27 +0500

    Разработанный контекст для товара Пешин

---
+++ Elements/Avia_Itm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pg_26.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Avia_Itm.xaml
+    /// </summary>
+    public partial class Avia_Itm : UserControl
+    {
+        public Avia_Itm()
+        {
+            InitializeComponent();
+        }
+    }
+}
