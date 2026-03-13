d64bc1c9ac7466ac2b8a3acb1fed69e3718672b7
Linashihova <linashikhova@mail.ru.>
2026-03-13 13:45:48 +0500

    Разроботка Понели Адменестиривания

--- Converters/HexToBrushConverter.cs
+++ Converters/HexToBrushConverter.cs
@@ -12,6 +12,7 @@ namespace WpfApp1.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is string hex)
+
             {
                 try
                 {
